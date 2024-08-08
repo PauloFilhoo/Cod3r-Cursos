44d65bf5f73068408b2163856e860b3b57f3ef8e
Paulo Filho <98671374+PauloFilhoo@users.noreply.github.com>
2024-08-08 18:06:39 -0300

    Exercicios e bootstrap

---
+++ exercicios-js/exercicios.js
@@ -0,0 +1,19 @@
+// Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação no final
+
+// function Ola(nome)
+// {
+//     console.log(`Olá ${nome}!`)
+// }
+
+// Ola('paulo')
+// Ola('Leonardo')
+
+// Escreva uma função que receba a idade de uma pessoa em anos e retorne em dias
+
+function ConveterIdade(idade)
+{
+    console.log(idade * 365)
+}
+
+ConveterIdade(25)
+ConveterIdade(16)
